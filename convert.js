ed041356a0d29ceb1fe7b93073ce5410e02d4494
danijelcecelja-svg <danijel.cecelja@vdab.be>
2026-09-09 09:53:09 +0200

    Refactor convert function to use convertLine

--- convert.js
+++ convert.js
@@ -1,14 +1,18 @@
-
 console.log("hello")
 
 function convertLine(line, lineNumber) {
-
-  
-
     const lowerLine = line.toLowerCase();
+    return lowerLine;
+}
 
-   
-            return lowerLine;
-   
-   
+function convert() {
+    const input = document.getElementById("input").value;
+    const lines = input.split(/\r?\n/);
+    const output = [];
+
+    lines.forEach((line, index) => {
+        output.push(convertLine(line, index + 1));
+    });
+
+    document.getElementById("output").value = output.join("\n");
 }
